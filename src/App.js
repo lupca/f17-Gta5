39453d56a3a5ec3f1989a29cce793026f4dde103
tungdt-3023 <dang.thanh.tung@sun-asterisk.com>
2022-08-04 15:33:57 +0700

    feat: user api

--- src/App.js
+++ src/App.js
@@ -4,9 +4,24 @@ import HomePage from "pages/HomePage";
 import Payment from "pages/Payment";
 import { Routes, Route } from "react-router-dom";
 import { theme } from "themes";
+import UserApi from './api/user.api'
+import React, { useEffect } from 'react';
 import "./App.scss";
 
 function App() {
+  // const [user, setUser] = useState([]);
+  useEffect(() => {
+    let mounted = true;
+    const user = new UserApi()
+    console.log(user.url)
+    user.setParamsToUrl().searchBy({citizenId: '/GXC48104'})
+    .then(data => {
+      console.log(data)
+    })
+    
+    return () => mounted = false;
+  }, [])
+
   return (
     <ThemeProvider theme={theme}>
       <Routes>
