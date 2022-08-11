c3ee65a64e903d7d94e52f638add44b1c8106abb
huyentrang93 <37203386+huyentrang93@users.noreply.github.com>
2022-08-11 19:55:24 +0700

    paypal
    paypal

--- src/api/payments/paypal.api.js
+++ src/api/payments/paypal.api.js
@@ -1,1 +1,23 @@
-// pending
+import BaseApi from '../base.api'
+
+export default class payPalWebhookApi extends BaseApi{
+    
+  constructor(){
+    super()
+    this.setSubdiretory("/api/v1/paypal/webhooks").url = this.domain + this.subdiretory
+  }
+  async loadPaymentPayPal({tranId, ackTime, amount, currency_code, status, partnerId, citizenid, fcoin} = {}) {
+    const data = {
+        tranId: tranId, 
+        ackTime: ackTime, 
+        amount: amount, 
+        currency_code: currency_code, 
+        status: status, 
+        partnerId: partnerId, 
+        citizenid: citizenid, 
+        fcoin: fcoin
+      }
+    this.setData(data)
+    return await this.postData()
+  }
+}
