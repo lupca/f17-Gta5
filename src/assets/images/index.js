4c1f9e1f4c1e2d182abe0b56cf9e8b7bd723a2d4
PhuongNguyen166 <julia.nguyen0616@gmail.com>
2022-08-12 07:51:04 +0700

    update-layout

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,13 +1,14 @@
 import logo from "./F17City.png";
-import HeroBanner from "./banner.jpg";
+import BgBanner from "./banner.jpg";
+import HeroBanner from "./BannerHero.png";
 import Momo from "./paymentMethods/momo.png";
 
 const Images = {
   LOGO: logo,
   HeroBanner,
   PaymentMethod: {
-    Momo: Momo
-  }
+    Momo: Momo,
+  },
 };
 
 export default Images;
--- src/pages/HomePage/HeroBanner/index.jsx
+++ src/pages/HomePage/HeroBanner/index.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import Images from "assets/images";
 import "./HeroBanner.scss";
-import { Collapse, Container } from "@mui/material";
-import { Link as Scroll } from "react-scroll";
+import {Collapse, Container} from "@mui/material";
+import {Link as Scroll} from "react-scroll";
 function HeroBanner(props) {
   return (
     <div
@@ -13,9 +13,7 @@ function HeroBanner(props) {
       }}
     >
       <Container maxWidth="xl">
-        <Collapse orientation="horizontal" in={true}>
-          <p className="hero-banner-title">THÀNH PHỐ F17</p>
-        </Collapse>
+
         <div id="scroll-down-animation">
           <Scroll to="features">
             <span className="mouse">
