63630ba7308b180f62120c6c52d597acd8552467
huyentrang93 <37203386+huyentrang93@users.noreply.github.com>
2022-08-15 18:28:52 +0700

    format

--- src/api/payments/paypal.api.js
+++ src/api/payments/paypal.api.js
@@ -6,16 +6,16 @@ export default class payPalWebhookApi extends BaseApi{
     super()
     this.setSubdiretory("/api/v1/paypal/webhooks").url = this.domain + this.subdiretory
   }
-  async loadPaymentPayPal({tranId, ackTime, amount, currency_code, status, partnerId, citizenid, fcoin} = {}) {
+  async loadPaymentPayPal({tranId, ackTime, amount, currencyCode, status, partnerId, citizenId, fcoin} = {}) {
     const data = {
-        tranId: tranId, 
-        ackTime: ackTime, 
-        amount: amount, 
-        currency_code: currency_code, 
-        status: status, 
-        partnerId: partnerId, 
-        citizenid: citizenid, 
-        fcoin: fcoin
+      tranId: tranId, 
+      ackTime: ackTime, 
+      amount: amount, 
+      currency_code: currencyCode, 
+      status: status, 
+      partnerId: partnerId, 
+      citizenid: citizenId, 
+      fcoin: fcoin
       }
     this.setData(data)
     return await this.postData()
