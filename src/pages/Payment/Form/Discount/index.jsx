790f1b624398ae244fadd107d9f7322db5614812
huyentrang93 <37203386+huyentrang93@users.noreply.github.com>
2022-08-17 23:14:22 +0700

    updatepaymentmomo

--- src/pages/Payment/Form/Discount/index.jsx
+++ src/pages/Payment/Form/Discount/index.jsx
@@ -5,11 +5,20 @@ import Images from "assets/images";
 
 const Discount = () => {
   return (
-    <Card variant="outlined" >
-    <div variant="outlined" className="payment-form__discount-momo">
-      <img  src={Images.PaymentMethod.Momo} alt="F17" />
-    </div>
-      Khuyến mãi 10% VND khi nạp trên 550 Fcoin (Áp dụng cho cả Paypal)
+    <Card  variant="outlined" >
+      <Card>
+        <div variant="outlined" className="payment-form__discount-title">
+          Nạp F17coin
+        </div>
+      </Card>
+      <Card>
+        <div variant="outlined" className="payment-form__discount-momo">
+          <img  src={Images.PaymentMethod.Momo} alt="F17" />
+          <p className='payment-form__momo-content'>
+            Khuyến mãi 10% VND khi nạp trên 550 Fcoin (Áp dụng cho cả Paypal)
+          </p>
+        </div>
+      </Card>
     </Card>
   );
 }
