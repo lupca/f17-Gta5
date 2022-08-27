d531f2326ee158dc5c1f6dd15001a82a656578a8
PhuongNguyen166 <julia.nguyen0616@gmail.com>
2022-08-27 12:49:24 +0700

    desktop ui

--- src/pages/HomePage/HowToJoin/components/DownloadLauncher.jsx
+++ src/pages/HomePage/HowToJoin/components/DownloadLauncher.jsx
@@ -1,27 +1,28 @@
-import {Grid} from '@mui/material'
 import React from 'react'
 import './Card.scss'
+import PropTypes from "prop-types";
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
 
-const DownloadLauncher = () => {
+const DownloadLauncher = ({onClick, isSelected}) => {
   return (
-    <div className="card">
-
+    <div className={`card card2 ${isSelected}`} onClick={onClick}>
       <div className='download-launcher'>
-        <p className="step">Bước 2</p>
-        <p className="step-title">
-          Tải laucher
-        </p>
-
-        <div className="button-action">
-          Tải xuống
+        <div className="step-download">
+          <p className="step">Bước 2</p>
+          <p className="step-title">
+            Tải laucher
+          </p>
         </div>
-
-
-
-
+        <div className="button-action" >
+          <span>Tải xuống</span>  <ArrowDownwardIcon fontSize="large" />
+        </div>
       </div>
     </div >
   )
 }
+DownloadLauncher.propTypes = {
+  onClick: PropTypes.func,
+  isSelected: PropTypes.string
+};
 
 export default DownloadLauncher
--- src/pages/HomePage/HowToJoin/components/Register.jsx
+++ src/pages/HomePage/HowToJoin/components/Register.jsx
@@ -1,39 +1,29 @@
-import {Grid} from '@mui/material'
-import React from 'react'
-import './Card.scss'
+import React from "react";
+import PropTypes from "prop-types";
+import "./Card.scss";
 
-const Register = () => {
+const Register = ({onClick, isSelected}) => {
   return (
-    <div className="card">
-
-      <div className='register'>
-        <p className="step">Bước 1</p>
-        <p className="step-title">
-          Đăng ký tài khoản
-        </p>
-        <Grid container spacing={3} direction="row"
-          style={{marginTop: 70}}
-          justifyContent="space-between"
-          alignItems="center">
-          <Grid item xs="auto">
-            <div className="button-action">
-              + Đăng ký ngay
-            </div>
-          </Grid>
-          <Grid item xs={7}>
-            <p className="step-content">
-              bạn cần có bản quyền
-              Grand theft auto v
-            </p>
-          </Grid>
-
-        </Grid>
-
-
+    <div className={`card card1 ${isSelected}`} onClick={onClick}>
+      <div className="register">
+        <div className="step-register">
+          <p className="step">Bước 1</p>
+          <p className="step-title">Đăng ký tài khoản</p>
+        </div>
+        <div className="action-register">
+          <div className="button-action">+ Đăng ký ngay</div>
+          <p className="step-content">
+            bạn cần có bản quyền Grand theft auto v
+          </p>
+        </div>
 
       </div>
-    </div >
-  )
-}
+    </div>
+  );
+};
+Register.propTypes = {
+  onClick: PropTypes.func,
+  isSelected: PropTypes.string
+};
 
-export default Register
+export default Register;
