1b6a9615e03f5d3d4000f4bd8ff8ba7e07a66b92
santahashi <freekill0113@gmail.com>
2022-10-28 18:42:35 +0700

    Update 3

--- src/components/ActionButton/ActionButton.jsx
+++ src/components/ActionButton/ActionButton.jsx
@@ -4,7 +4,7 @@ import './ActionButton.scss'
 
 export const ActionButton = () => {
     const handleActionButton = () => {
-        window.open("https://cfx.re/join/b3bgbd/");
+        window.open("https://cfx.re/join/b3bgbd");
     };
 
   return (
